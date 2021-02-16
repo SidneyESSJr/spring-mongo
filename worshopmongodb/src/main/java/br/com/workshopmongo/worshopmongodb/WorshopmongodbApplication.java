3d079bdb58f3eca82cf3b282a108bfc0a3c4fc71
Sidney Espírito Santo de Souza Júnior <sidney.e.s.s.jr@gmail.com>
2021-02-16 08:51:31 -0300

    Projeto criado

---
+++ worshopmongodb/src/main/java/br/com/workshopmongo/worshopmongodb/WorshopmongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.workshopmongo.worshopmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorshopmongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorshopmongodbApplication.class, args);
+	}
+
+}
---
+++ worshopmongodb/src/test/java/br/com/workshopmongo/worshopmongodb/WorshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.workshopmongo.worshopmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
