ff0468154f93c91cb4f7981d1ccdd6d80d2bf46d
Sidney.E.S.S.Jr <sidney.E.S.S.Jr@gmail.com>
2021-02-22 12:18:24 -0300

    Acrescentando comentários aos posts

--- worshopmongodb/src/main/java/br/com/workshopmongo/worshopmongodb/config/Instantiation.java
+++ worshopmongodb/src/main/java/br/com/workshopmongo/worshopmongodb/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.workshopmongo.worshopmongodb.domain.Post;
 import br.com.workshopmongo.worshopmongodb.domain.User;
 import br.com.workshopmongo.worshopmongodb.dto.AuthorDTO;
+import br.com.workshopmongo.worshopmongodb.dto.CommentDTO;
 import br.com.workshopmongo.worshopmongodb.repository.PostRepository;
 import br.com.workshopmongo.worshopmongodb.repository.UserRepository;
 
@@ -39,9 +40,17 @@ public class Instantiation implements CommandLineRunner {
 
         uRepository.saveAll(Arrays.asList(maria, alex, bob));
 
-        Post post1 = new Post(null, sdf.parse("21/02/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post1 = new Post(null, sdf.parse("21/02/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!",
+                new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
-        
+
+        CommentDTO comment1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Aproveite!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
+
         pRepository.saveAll(Arrays.asList(post1, post2));
 
         maria.getPosts().addAll(Arrays.asList(post1, post2));
--- worshopmongodb/src/main/java/br/com/workshopmongo/worshopmongodb/domain/Post.java
+++ worshopmongodb/src/main/java/br/com/workshopmongo/worshopmongodb/domain/Post.java
@@ -1,12 +1,15 @@
 package br.com.workshopmongo.worshopmongodb.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.workshopmongo.worshopmongodb.dto.AuthorDTO;
+import br.com.workshopmongo.worshopmongodb.dto.CommentDTO;
 
 @Document(collection = "post")
 public class Post implements Serializable {
@@ -19,6 +22,8 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -63,12 +68,20 @@ public class Post implements Serializable {
         this.body = body;
     }
 
-    public User getAuthor() {
-        return author.toUser();
+    public AuthorDTO getAuthor() {
+        return author;
     }
 
     public void setAuthor(AuthorDTO author) {
         this.author = author;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<CommentDTO> comments) {
+        this.comments = comments;
     }
 
     @Override
